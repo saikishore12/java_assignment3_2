1bd56715f85aae717c56b7d6f6ccce6607d93525
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-09-24 21:28:36 +0530

    Assignment

---
+++ src/Assignment2.java
@@ -0,0 +1,13 @@
+
+public class Assignment2 {
+	public static void main(String[] args) {
+		String s1 = args[0];
+		String s2 = args[1];
+		boolean same = false;
+		if (s1.equals(s2)) {
+			same = true;
+
+		}
+		System.out.println(same);
+	}
+}
